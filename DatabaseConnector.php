2a8ff46b1d706f2433a83bdc9d3fecb67dada4e1
Wesley Erasmus <wesley.erasmus119@gmail.com>
2023-02-19 17:50:13 +0200

    Headers (CORS error)

--- DatabaseConnector.php
+++ DatabaseConnector.php
@@ -1,4 +1,8 @@
 <?php
+header('Access-Control-Allow-Origin: *'); // Allows access from any origin
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS'); // Allows the specified HTTP methods
+header('Access-Control-Allow-Headers: Content-Type, Authorization'); // Allows the specified headers to be sent in the request
+
 // DatabaseConnector class
 class DatabaseConnector
 {
